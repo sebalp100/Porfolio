7be99581e33630120f9cde2070951dcd7568e91e
sebalp100 <sebalp100@gmail.com>
2023-05-13 18:18:33 -0300

    Add slider and projects initial data

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,10 +1,30 @@
+'use client';
 import React from 'react';
+import Carousel from 'nuka-carousel';
+import data from './data/data';
+import Image from 'next/image';
 
 const Projects = () => {
+  const settings = {
+    slidesToShow: 3,
+  };
+
   return (
-    <section id="projects" className="flex h-screen bg-slate-950 w-full flex-col justify-center items-center">
-      <h1 className="text-4xl mb-5 font-bold">Projects</h1>
-      <span className="text-7xl">💼</span>
+    <section
+      id="projects"
+      className="flex text-white h-screen bg-slate-950 sm:pl-40 p-20 flex-col justify-center items-center"
+    >
+      <Carousel {...settings}>
+        {data.map((room) => (
+          <li key={room.id} className="bg-gray-900">
+            <Image alt="Project" src={room.image} width={1118} height={749} />
+            <h4 className="roomNames">{room.title}</h4>
+            <span className="separator">. . . . . . . . . . . . . .</span>
+            <p>{room.description}</p>
+            <div>{room.icons}</div>
+          </li>
+        ))}
+      </Carousel>
     </section>
   );
 };
---
+++ components/data/data.js
@@ -0,0 +1,47 @@
+const { FaReact } = require("react-icons/fa");
+const { SiRedux, SiRubyonrails } = require("react-icons/si");
+
+const data = [
+  {
+    id: 1,
+    title: 'Hotel Bookings',
+    description: 'This is a Hotel bookings app that allows users to search a list of rooms availables and make reservations for 24hs. It also allows user to add and remove rooms.',
+    icons: <><FaReact /><SiRedux /><SiRubyonrails /></>,
+    image: "/hotelb.png",
+    link: 'https://github.com/sebalp100/hotel-bookings-frontend'
+  },
+  {
+    id: 2,
+    title: 'Hotel Bookings',
+    description: 'This is a Hotel bookings app that allows users to search a list of rooms availables and make reservations for 24hs. It also allows user to add and remove rooms.',
+    icons: <><FaReact /><SiRedux /><SiRubyonrails /></>,
+    image: '/hotelb.png',
+    link: 'https://github.com/sebalp100/hotel-bookings-frontend'
+  },
+  {
+    id: 3,
+    title: 'Hotel Bookings',
+    description: 'This is a Hotel bookings app that allows users to search a list of rooms availables and make reservations for 24hs. It also allows user to add and remove rooms.',
+    icons: <><FaReact /><SiRedux /><SiRubyonrails /></>,
+    image: '/hotelb.png',
+    link: 'https://github.com/sebalp100/hotel-bookings-frontend'
+  },
+  {
+    id: 4,
+    title: 'Hotel Bookings',
+    description: 'This is a Hotel bookings app that allows users to search a list of rooms availables and make reservations for 24hs. It also allows user to add and remove rooms.',
+    icons: <><FaReact /><SiRedux /><SiRubyonrails /></>,
+    image: '/hotelb.png',
+    link: 'https://github.com/sebalp100/hotel-bookings-frontend'
+  },
+  {
+    id: 5,
+    title: 'Hotel Bookings',
+    description: 'This is a Hotel bookings app that allows users to search a list of rooms availables and make reservations for 24hs. It also allows user to add and remove rooms.',
+    icons: <><FaReact /><SiRedux /><SiRubyonrails /></>,
+    image: '/hotelb.png',
+    link: 'https://github.com/sebalp100/hotel-bookings-frontend'
+  }
+]
+
+export default data;
