ccde8c0ca16efc249c409d1a63abb5da105fd51d
sebalp100 <sebalp100@gmail.com>
2023-12-09 20:56:09 -0300

    Update project list

--- components/data/data.js
+++ components/data/data.js
@@ -6,19 +6,32 @@ const {
 const data = [
   {
     id: 1,
+    title: 'Rent a Car',
+    description: 'This is a Car rental app that allows users to search a list of cars available and rent them, choosing a starting and a returning date. Admins can add and remove cars.',
+    icons: <>
+      <FaReact />
+      <SiRedux />
+      <SiRubyonrails className="railsIcon text-gray-950" />
+    </>,
+    image: '/renthome.png',
+    link: 'https://github.com/sebalp100/rent-a-car-backend',
+    demo: 'https://rent-a-car-frontend-sebalp100.vercel.app/',
+  },
+  {
+    id: 2,
     title: 'Hotel Bookings',
     description: 'This is a Hotel bookings app that allows users to search a list of rooms availables and make reservations for 24hs. It also allows user to add new rooms or remove them.',
     icons: <>
       <FaReact />
       <SiRedux />
       <SiRubyonrails className="railsIcon text-gray-950" />
-           </>,
+    </>,
     image: '/hotelb.png',
     link: 'https://github.com/sebalp100/hotel-bookings-frontend',
     demo: 'https://hotel-bookings-frontend.vercel.app',
   },
   {
-    id: 2,
+    id: 3,
     title: 'BudgetMate',
     description: 'Mobile web application where you can manage your budget: you have a list of transactions associated with a category, so that you can see how much money you spent and on what.',
     icons: <>
@@ -30,7 +43,7 @@ const data = [
     demo: 'https://budgetmate.onrender.com/',
   },
   {
-    id: 3,
+    id: 4,
     title: 'Bookstore',
     description: 'Bookstore: Simple app made with React that allows users to create and remove books, and displays a list of them. The app uses Redux to create a global state and store the data.',
     icons: <>
@@ -42,7 +55,7 @@ const data = [
     demo: 'https://simple-bookstore.onrender.com/',
   },
   {
-    id: 4,
+    id: 5,
     title: 'Rails Blog App',
     description: 'This is a simple blog app made in Rails that will show the list of posts and empower readers to interact with them by adding comments and liking posts. It also has active storage.',
     icons: <>
@@ -54,20 +67,20 @@ const data = [
     demo: 'https://rails-production-245a.up.railway.app/',
   },
   {
-    id: 5,
+    id: 6,
     title: 'Space Travelers',
     description: 'This is a website that allows users to reserve and book their favorite rockets, missions and dragons spaceships. The app displays real live data from the SpaceX API using a Redux store.',
     icons: <>
       <FaReact />
       <SiRedux />
       <SiTailwindcss />
-           </>,
+    </>,
     image: '/spacetra.png',
     link: 'https://github.com/sebalp100/space-travelers',
     demo: 'https://space-travelers-iyfi.onrender.com/',
   },
   {
-    id: 6,
+    id: 7,
     title: 'Moons of the Solar system',
     description: 'This is a simple mobile website that allows users to search for all the moons in the solar system for each planet. It uses real data from The Solar System OpenData API and a Redux store.',
     icons: <>
@@ -79,7 +92,7 @@ const data = [
     demo: 'https://moons-of-the-solar-system.onrender.com/',
   },
   {
-    id: 7,
+    id: 8,
     title: 'Simple Chat',
     description: 'This is a website that allows users to chat with one another using real-time Firebase data and React hooks. The app allows users to look up their previous conversations with other people.',
     icons: <>
@@ -91,7 +104,7 @@ const data = [
     demo: 'https://simple-chat-app-puqe.onrender.com/',
   },
   {
-    id: 8,
+    id: 9,
     title: 'New Movies',
     description: 'Website that has a desktop and mobile version and allows users to interact by giving a like to their favorite movies and checking for other users comments, as well as adding their own.',
     icons: <>
@@ -100,13 +113,13 @@ const data = [
       {' '}
       <SiWebpack />
       {' '}
-           </>,
+    </>,
     image: '/newmovies.png',
     link: 'https://github.com/YKerroum/New-movies',
     demo: 'https://ykerroum.github.io/New-movies/dist/',
   },
   {
-    id: 9,
+    id: 10,
     title: 'Contactology Without Borders',
     description: 'Conference page created with the mobile-first approach for the Home and About sections. Using JavaScript, I generated an "Speakers" section by accesing the data stored in an JS Object.',
     icons: <>
@@ -115,7 +128,7 @@ const data = [
       {' '}
       <SiCss3 />
       {' '}
-           </>,
+    </>,
     image: '/contacto.png',
     link: 'https://github.com/sebalp100/Module-1-capstone-project',
     demo: 'https://sebalp100.github.io/Module-1-capstone-project/html/main.html',
